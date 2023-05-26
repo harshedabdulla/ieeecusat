8c475f5cfda2fe44bb822cc39f2c2dc32fbc9f59
Eldhose <112414538+Vikk20@users.noreply.github.com>
2023-05-26 13:11:04 +0530

    Update Home.jsx

--- ieee2023/src/components/Home.jsx
+++ ieee2023/src/components/Home.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Navbar from './Navbar'
-import Footer from './Footer'
+
 
 function Home() {
   return (
     <div>
        <Navbar/>
-       <Footer/>
     </div>
   )
 }
