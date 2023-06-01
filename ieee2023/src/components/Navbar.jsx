4e8483f2d9ab0a1723942ace0c790520dfb303fb
harshedabdulla <harshed2004@gmail.com>
2023-06-01 15:17:23 +0530

    deleted package

--- ieee2023/src/components/Navbar.jsx
+++ ieee2023/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-white pt-3">
-      <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
+      <div className="max-w-8xl mx-auto px-2 sm:px-4 lg:px-8">
         <div className="flex justify-between h-8">
           <div className="flex-shrink-0 flex items-center">
             <img src={logo4} alt="IEEE Icon" className="h-9 mt-2"/>
