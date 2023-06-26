4d7d8fa4913ff8e530808b8b38efe1099e6b2919
harshedabdulla <harshed2004@gmail.com>
2023-06-26 10:08:37 +0530

    navbars tweaks 2

--- ieee2023/src/components/Navbar.jsx
+++ ieee2023/src/components/Navbar.jsx
@@ -135,7 +135,24 @@ const Navbar = () => {
         variants={menuVariants}
         transition={{ duration: 0.3 }}
       >
-       <div className="w-full max-w-sm px-2 pt-4 pb-3 space-y-1 sm:px-3 shadow-sm">
+        <div className="w-full max-w-sm px-2 pt-4 pb-3 space-y-1 sm:px-3 shadow-sm">
+          <button
+            type="button"
+            className="bg-white inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
+            onClick={toggleMenu}
+          >
+            <span className="sr-only">Close menu</span>
+            <svg
+              className="h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
           <Link to="/" className="hover:text-gray-900 block px-3 py-2 text-base">Home</Link>
           <HashLink to="/#about" className="hover:text-gray-900 block px-3 py-2 text-base">About</HashLink>
           <HashLink to="/events" className="hover:text-gray-900 block px-3 py-2 text-base">Events</HashLink>
