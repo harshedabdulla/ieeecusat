264903b7de32831b50f4e8b1613032493ed4cdd5
harshedabdulla <harshed2004@gmail.com>
2023-08-31 22:36:43 +0530

    s

--- ieee2023/src/components/Events.jsx
+++ ieee2023/src/components/Events.jsx
@@ -3,11 +3,16 @@ import Navbar from './Navbar'
 import Title from './Title'
 import Flagship from './Flagship'
 import EventCard from './EventCard';
+import aksc from '../assets/event/aksc.jpeg';
+import back from '../assets/event/back.jpeg';
+import congo from '../assets/event/congo.jpeg';
+import sight from '../assets/event/sight.jpeg';
+import tthunt from '../assets/event/tthunt.jpeg';
 
 
 function Events() {
   const eventData = {
-    poster: 'https://via.placeholder.com/300',
+    poster: back,
     status: 'Upcoming',
     date: '24',
     month: 'May',
@@ -17,7 +22,7 @@ function Events() {
     date: '2nd June 2023',
     mode: 'Offline',
     registerLink: 'https://example.com/register',
-    imageSrc: 'https://via.placeholder.com/800x600',
+    imageSrc: aksc,
   };
   return (
     <div>
@@ -25,6 +30,9 @@ function Events() {
         <Title title="Events" />
         <Flagship event={flagshipEvent} />
         <div className="flex flex-wrap justify-center mx-auto">
+          <h3 className="text-3xl text-orange-400 text-center mb-12">Past Events</h3>
+        <div className="flex flex-wrap justify-center mx-auto">
+        
   <div className="lg:w-1/3 lg:px-4 md:w-1/2 md:px-2 mb-8 mx-auto">
     <EventCard {...eventData} />
   </div>
@@ -33,6 +41,7 @@ function Events() {
   </div>
   <div className="lg:w-1/3 lg:px-4 md:w-1/2 md:px-2 mb-8 mx-auto">
     <EventCard {...eventData} />
+  </div>
   </div>
 </div>
 
