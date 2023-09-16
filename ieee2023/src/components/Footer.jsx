f8287de40469b669a5ea948154d888193108af8d
harshedabdulla <harshed2004@gmail.com>
2023-09-16 13:06:19 +0530

    height

--- ieee2023/src/components/Footer.jsx
+++ ieee2023/src/components/Footer.jsx
@@ -9,10 +9,10 @@ const Footer = () => {
         <footer className="footer w-full flex flex-col gap-4 px-2 md:px-16 py-2">
             <div className='flex flex-col md:flex-row gap-2'>
                 <div className='w-full md:w-1/3 h-full flex flex-col gap-4'>
-                    <div className='flex justify-center items-center h-full gap-4'>
+                    <div className='flex justify-center items-center h-full gap-2'>
                         <img src={logo} alt="cusat" />
-                        <div className='w-[0.2rem] bg-white h-3/4 rounded-lg'></div>
-                        <img src={logo2} alt="cusat" />
+                        
+                        
                     </div>
                     <div className='footer__card max-w-[400px] p-10 text-white flex gap-2 flex-col'>
                         <p>We're a team focused on creative and innovative solutions.</p>
